5f35767a5ea55d489a72e7533f3ccfea5348a9d5
Nithish Ariyha <nithishariyha02467@gmail.com>
2025-08-16 23:50:12 +0530

    o

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // For GitHub Pages project site (https://ariyha.github.io/portfolio)
-  base: '.',
   plugins: [react()],
-  build: {
-    // gh-pages script in package.json expects a `build` folder
-    outDir: 'build'
-  },
   server: {
     port: 5173,
     host: true
